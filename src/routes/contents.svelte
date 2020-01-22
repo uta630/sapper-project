f484b1717e7ad9430552918f2cf1c64aa5336ba9
Usui Takeshi <usuitkc.630@gmail.com>
2020-01-22 12:45:24 +0900

    ページ:/contents 作成

---
+++ src/routes/contents.svelte
@@ -0,0 +1,17 @@
+<script>
+let time = new Date();
+setInterval(() => {
+		time = new Date();
+	}, 1000
+)
+</script>
+
+<svelte:head>
+	<title>contents</title>
+</svelte:head>
+
+<h1>contents this site</h1>
+
+<p>This is the 'contents' page.</p>
+
+<p>{time}</p>
